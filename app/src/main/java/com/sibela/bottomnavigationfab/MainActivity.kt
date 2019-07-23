fa94c87dbce5ea95cfe6f17b10822846def674cb
leonardosibela <leonardo_sibela@hotmail.com>
2019-07-23 10:15:11 -0300

    Adding layout and layout flow

--- app/src/main/java/com/sibela/bottomnavigationfab/MainActivity.kt
+++ app/src/main/java/com/sibela/bottomnavigationfab/MainActivity.kt
@@ -2,11 +2,27 @@ package com.sibela.bottomnavigationfab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomSheetBehavior: BottomSheetBehavior<MaterialCardView> by lazy {
+        BottomSheetBehavior.from(bottomSheet)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomSheetBehavior.isHideable = true
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        layersFab.setOnClickListener { openBottomSheet() }
+    }
+
+    private fun openBottomSheet() {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
     }
 }
